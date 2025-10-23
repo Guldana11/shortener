0b7a59dd351a711dca6cd637757b25566e700ed5
Ramazan <ramadansmailov77@gmail.com>
2025-10-23 14:34:54 +0500

    Fea: задача по инкр5

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
@@ -10,13 +11,26 @@ type Config struct {
 }
 
 func Init() *Config {
-	address := flag.String("a", "127.0.0.1:8080", "адрес запуска HTTP-сервера")
-	baseURL := flag.String("b", "http://localhost:8080", "базовый адрес сокращённого URL")
+	addressFlag := flag.String("a", "127.0.0.1:8080", "адрес запуска HTTP-сервера")
+	baseURLFlag := flag.String("b", "http://localhost:8080", "базовый адрес сокращённого URL")
 
 	flag.Parse()
 
+	addressEnv := os.Getenv("SERVER_ADDRESS")
+	baseURLEnv := os.Getenv("BASE_URL")
+
+	address := *addressFlag
+	baseURL := *baseURLFlag
+
+	if addressEnv != "" {
+		address = addressEnv
+	}
+	if baseURLEnv != "" {
+		baseURL = baseURLEnv
+	}
+
 	return &Config{
-		Address: *address,
-		BaseURL: *baseURL,
+		Address: address,
+		BaseURL: baseURL,
 	}
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -11,11 +11,13 @@ func TestInit(t *testing.T) {
 	tests := []struct {
 		name       string
 		args       []string
+		env        map[string]string
 		wantConfig *Config
 	}{
 		{
 			name: "Значения по умолчанию",
 			args: []string{"cmd"},
+			env:  nil,
 			wantConfig: &Config{
 				Address: "127.0.0.1:8080",
 				BaseURL: "http://localhost:8080",
@@ -24,18 +26,43 @@ func TestInit(t *testing.T) {
 		{
 			name: "Переопределение через флаги",
 			args: []string{"cmd", "-a", "127.0.0.1:9999", "-b", "http://example.com"},
+			env:  nil,
 			wantConfig: &Config{
 				Address: "127.0.0.1:9999",
 				BaseURL: "http://example.com",
+			},
+		},
+		{
+			name: "Переопределение через переменные окружения",
+			args: []string{"cmd", "-a", "127.0.0.1:9999", "-b", "http://example.com"},
+			env: map[string]string{
+				"SERVER_ADDRESS": "0.0.0.0:3000",
+				"BASE_URL":       "http://myshort.io",
+			},
+			wantConfig: &Config{
+				Address: "0.0.0.0:3000",
+				BaseURL: "http://myshort.io",
 			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			oldServerAddr := os.Getenv("SERVER_ADDRESS")
+			oldBaseURL := os.Getenv("BASE_URL")
+			defer func() {
+				os.Setenv("SERVER_ADDRESS", oldServerAddr)
+				os.Setenv("BASE_URL", oldBaseURL)
+			}()
+
+			os.Unsetenv("SERVER_ADDRESS")
+			os.Unsetenv("BASE_URL")
+			for k, v := range tt.env {
+				os.Setenv(k, v)
+			}
+
 			oldArgs := os.Args
 			defer func() { os.Args = oldArgs }()
-
 			os.Args = tt.args
 
 			flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
